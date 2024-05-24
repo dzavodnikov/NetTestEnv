dafbf816345bd6e30570e2872b650ef6d396ec72
Dmitry Zavodnikov <d.zavodnikov@gmail.com>
2024-05-24 13:40:05 +0200

    Add NAT for network 2 and 3

--- init/client3.sh
+++ init/client3.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # IP confugure (see `route -n`).
-route delete    default gw 192.168.0.1 eth0
-route add       default gw 192.168.0.2 eth0
+route add default gw 192.168.0.2 eth0
 
 # Create test service.
 python /opt/hostname_server.py --port 8080
--- init/router.sh
+++ init/router.sh
@@ -7,5 +7,9 @@ ip address add 10.0.0.156/29 dev eth0
 ip address add 10.0.0.157/29 dev eth0
 ip address add 10.0.0.158/29 dev eth0
 
+# Configure NAT.
+iptables -t nat -A POSTROUTING  -o eth0 -j MASQUERADE
+#iptables        -A FORWARD      -i eth1 -j ACCEPT
+
 # Create test service.
 python /opt/hostname_server.py --port 8080
